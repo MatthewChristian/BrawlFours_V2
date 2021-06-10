a8e65c2f06a6b8d10fed8ca8957c1de1d08b0c55
MatthewChristian <matthewjlchristian@yahoo.com>
2021-06-09 23:41:27 -0400

    Card images now render to screen

--- components/PlayingCard.js
+++ components/PlayingCard.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect, useRef } from 'react';
+import Image from 'next/image'
 
 export default function PlayingCard(props) {
-
+  
     return (
         <div className="col-sm-1">
-          <div className="card">
-            <p>{props.value}</p>
-          </div>
+          
+          <Image
+            src={`/images/${props.value}.png`}
+            width={140}
+            height={190}
+          />
+
         </div>
     )
 }
