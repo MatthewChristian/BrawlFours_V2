928e95340848508120c23a3e29823c5b1ee2ad14
MatthewChristian <matthewjlchristian@yahoo.com>
2021-09-11 18:02:57 -0400

    Implement ability to join rooms

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const handle = nextApp.getRequestHandler()
 
 let port = 3000
 let count = 0;
+var gameSocket;
 
 io.on('connect', socket => {
   count = count + 1;
@@ -20,10 +21,15 @@ io.on('connect', socket => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  gameSocket = socket;
   socket.on('createRoom', createRoom);
+  socket.on('joinRoom', joinRoom);
+  socket.on('playerJoinedRoom', playerJoinedRoom)
 });
 
-
+function logMapElements(value, key, map) {
+  console.log(`m[${key}] = ${value}`);
+}
 
 function createRoom() {
   // Create a unique numbered room
@@ -37,6 +43,41 @@ function createRoom() {
   // Join the Room and wait for the players
   this.join(thisRoomId.toString());
 };
+
+function joinRoom(data) {
+  // A reference to the player's Socket.IO socket object
+  var playerSocket = this;
+
+  // Look up the room ID in the Socket.IO manager object.
+
+  /*var rooms = gameSocket.adapter.rooms;
+  console.log("Len: " + gameSocket.adapter.rooms.size)
+  gameSocket.adapter.rooms.forEach(logMapElements);
+  console.log("RID: " + gameSocket.adapter.rooms.get('123'));*/
+
+  // If the room exists...
+  if(gameSocket.adapter.rooms.get(data.roomId)){
+      // attach the socket id to the data object.
+      data.mySocketId = playerSocket.id;
+
+      // Join the room
+      playerSocket.join(data.roomId);
+
+      console.log('Player joining game: ' + data.roomId );
+
+      // Emit an event notifying the clients that the player has joined the room.
+      gameSocket.in(data.roomId).emit('playerJoinedRoom', data);
+
+  } else {
+      // Otherwise, send an error message back to the player.
+      this.emit('error',{message: "This room does not exist."} );
+      console.log("Room doesnt exist");
+  }
+}
+
+function playerJoinedRoom () {
+  console.log('Player joined room');
+}
 
 nextApp.prepare()
 .then(() => {
