042477fc6b6a7aa39b156db7cedc0ea3aa4ad489
MatthewChristian <matthewjlchristian@yahoo.com>
2021-09-11 22:58:49 -0400

    Added functionality and some CSS to lobby buttons

--- components/Lobby.js
+++ components/Lobby.js
@@ -17,13 +17,34 @@ export default function Lobby(props) {
     // Store room ID of game that player created
     const [ createdRoomId, setCreatedRoomId ] = useState("");
 
+    // Used to open and close modal form
+    const [createdOpen, setCreatedOpen] = useState(false);
+    const closeCreatedModal = () => setCreatedOpen(false);
+
+    const [joinOpen, setJoinOpen] = useState(false);
+    const closeJoinModal = () => setJoinOpen(false);
+
+    // React ref to access text field values
+    const joinRoomRef = useRef(null)
+    const joinNickRef = useRef(null)
+
+    function createRoomPressed() {
+        createRoom();
+        setCreatedOpen(true);
+    }
+
+    function joinRoomPressed() {
+        setJoinOpen(true);
+    }
+
     // Create a room channel
     function createRoom(e) {
        
         socket.emit('createRoom');
 
         socket.on('newRoomCreated', data => {
-            console.log("Loaded: " + data.roomId)
+            console.log("Loaded: " + data.roomId);
+            setCreatedRoomId(data.roomId);
         })
     }
 
@@ -33,9 +54,13 @@ export default function Lobby(props) {
 
 
     function joinRoom(value) {
-       
+        const roomIdVal = joinRoomRef.current.value
+        const nickVal = joinNickRef.current.value
+        console.log("Room val: " + roomIdVal);
+        console.log("Nick val: " + nickVal);
         var data = {
-            roomId : '123'
+            roomId : String(roomIdVal),
+            nickname: String(nickVal)
         };
         socket.emit('joinRoom', data);
     }
@@ -58,13 +83,40 @@ export default function Lobby(props) {
     return (
         <div>
             <h1>Brawl Fours</h1>
-            <Popup trigger={<button onClick={(e) => createRoom()}> Create Lobby </button>} modal>
-                <div>Room created!</div>
-                <div>Share this code with your friends:</div>
-                <div></div>
+
+            <input type="text" className="" id="join-nickname-field" ref={joinNickRef} placeholder="Enter your nickname" />
+            <br></br>
+
+            
+            <button className="game-button join-button lobby-button" onClick={(e) => joinRoomPressed()}> 
+                Join Room 
+            </button>
+
+            <Popup open={joinOpen} closeOnDocumentClick onClose={closeJoinModal}>
+                <div className="join-room-h1 room-modal">Enter room code:</div>
+                <input type="text" className="" id="join-room-field" ref={joinRoomRef} placeholder="Enter the room code" />
+                <br></br>
+                <button className="game-button join-button lobby-button" onClick={(e) => joinRoom()}> 
+                    Join
+                </button>
             </Popup>
-            <button onClick={(e) => createRoom()}> Create Lobby2 </button>
-            <button onClick={(e) => joinRoom()}> Join Lobby </button>
+
+            
+
+            <br></br>
+
+            <button type="button" className="game-button create-button lobby-button" onClick={() => createRoomPressed()}>
+                Create Room
+            </button>
+            
+            <Popup open={createdOpen} closeOnDocumentClick onClose={closeCreatedModal}>
+                <div className="room-created-h1 room-modal">Room created!</div>
+                <div className="room-created-h2 room-modal">Share this code with your friends:</div>
+                <div className="room-created-id room-modal">{createdRoomId}</div>
+            </Popup>
+            
+
+
         </div>
     )
 }
