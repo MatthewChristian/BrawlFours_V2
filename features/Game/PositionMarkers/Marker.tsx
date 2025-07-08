2b126766fc1544646d33244bc7bf842ce287518e
MatthewChristian <matthewjlchristian@yahoo.com>
2025-07-07 21:02:35 -0400

    Remove unnecessary import

--- features/Game/PositionMarkers/Marker.tsx
+++ features/Game/PositionMarkers/Marker.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useAppDispatch } from '../../../store/hooks';
 import { UnknownAction } from '@reduxjs/toolkit';
 import { MarkerPosition } from '../../../models/MarkerPosition';
