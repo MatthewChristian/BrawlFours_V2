93fb284f2c0bfa119bd01365379150b843221903
MatthewChristian <matthewjlchristian@yahoo.com>
2025-07-15 18:37:42 -0400

    Add opposite power tooltip extra info, add trump icon

--- features/Game/CardInfoTooltip.tsx
+++ features/Game/CardInfoTooltip.tsx
@@ -9,6 +9,7 @@ import powerZeroSvg from '../../public/images/powerZero.svg';
 import { useAppSelector } from '../../store/hooks';
 import { getActiveAbilities, getIsMobile, getMobileView, getTwosPlayed } from '../../slices/game.slice';
 import { oppositePowerMap } from '../../core/services/sharedGameFunctions';
+import { FaCrown } from 'react-icons/fa';
 
 interface Props {
   tooltipRef: RefObject<TooltipRefProps>;
@@ -99,19 +100,30 @@ export default function CardInfoTooltip({ tooltipRef, card, active, offsetY, ign
         delayShow={mobileView ? 0 : 550}
         delayHide={mobileView ? 0 : 150}
         offset={(offsetY ?? 0) + 10}
-        style={{ zIndex: 20000, minWidth: 200, maxWidth: mobileView ? '80dvw' : 200 }}
+        style={{ zIndex: 20000, minWidth: 250, maxWidth: mobileView ? '80dvw' : 250 }}
         openOnClick={isMobile}
         className={'border border-white'}
         classNameArrow={'border border-white border-t-0 border-l-0'}
       >
         <div className='flex flex-col gap-2'>
-          <div className='flex flex-row gap-1 items-baseline'>
-            <div className='relative top-1 w-5'>
-              <Image priority
-                src={card.power == 0 ? powerZeroSvg : powerSvg}
-                alt="" />
+          <div className='grid grid-cols-2 gap-2'>
+            <div className='flex flex-row gap-1 items-baseline'>
+              <div className='relative top-1 w-5'>
+                <Image priority
+                  src={card.power == 0 ? powerZeroSvg : powerSvg}
+                  alt="" />
+              </div>
+              <div className={card.power == 0 ? 'text-red-500' : 'text-green-500'}>{cardPower} Power</div>
             </div>
-            <div className={card.power == 0 ? 'text-red-500' : 'text-green-500'}>{cardPower} Power</div>
+
+            { card.trump &&
+              <div className='flex flex-row gap-1 items-baseline'>
+                <div className='relative top-1 w-5'>
+                  <FaCrown color='#facc15' size={20} />
+                </div>
+                <div className={'text-yellow-400'}>Trump</div>
+              </div>
+            }
           </div>
 
           <div className='border-t-2 border-white'></div>
@@ -133,16 +145,6 @@ export default function CardInfoTooltip({ tooltipRef, card, active, offsetY, ign
             {abilityDescription}
           </div>
 
-
-          {
-            card?.isRandom ?
-              <div className='text-xs text-gray-400 italic'>
-                This card&apos;s ability is random every game
-              </div>
-              :
-              null
-          }
-
           {
             card?.ability == CardAbilities.twoWinGame ?
               <div className='text-xs text-gray-400 italic'>
@@ -155,6 +157,22 @@ export default function CardInfoTooltip({ tooltipRef, card, active, offsetY, ign
               <div className='text-xs text-gray-400 italic'>
                 If the chosen player has more cards, then one random card in their hand will not be swapped
               </div> : null
+          }
+
+          {
+            card?.ability == CardAbilities.oppositePower ?
+              <div className='text-xs text-gray-400 italic'>
+                Trump still beats non-Trump
+              </div> : null
+          }
+
+          {
+            card?.isRandom ?
+              <div className='text-xs text-gray-400 italic'>
+                This card&apos;s ability is random every game
+              </div>
+              :
+              null
           }
         </div>
 
