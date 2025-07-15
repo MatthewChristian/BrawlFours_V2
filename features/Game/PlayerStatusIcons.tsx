0bdadfa4da3dcdc652c1a6ea7da1d383d73970f8
MatthewChristian <matthewjlchristian@yahoo.com>
2025-07-15 18:23:22 -0400

    Fix player status icon animations

--- features/Game/PlayerStatusIcons.tsx
+++ features/Game/PlayerStatusIcons.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import HangSaverIcon from './StatusIcons/HangSaverIcon';
 import { CardAbilities } from '../../core/services/abilities';
 import AllyPlaysLastIcon from './StatusIcons/AllyPlaysLastIcon';
@@ -8,6 +8,7 @@ import NextCardTrumpIcon from './StatusIcons/NextCardTrumpIcon';
 import DealerIcon from './StatusIcons/DealerIcon';
 import TurnIcon from './StatusIcons/TurnIcon';
 import { PlayerSocket } from '../../models/PlayerSocket';
+import { AnimatePresence } from 'framer-motion';
 
 interface Props {
   playerStatus: CardAbilities[];
@@ -18,15 +19,28 @@ interface Props {
 }
 
 export default function PlayerStatusIcons({ playerStatus, dealerData, turnPlayerData, playerData, className }: Props) {
+
+  const activeItems = useMemo(() => {
+    return [
+      <DealerIcon key='di' active={dealerData && playerData.id == dealerData.id} />,
+      <TurnIcon key='ti' active={turnPlayerData && playerData.id == turnPlayerData.id} />,
+      <TwoWinGameIcon key='twg' active={playerStatus.includes(CardAbilities.twoWinGame)} />,
+      <NextCardTrumpIcon key='nct' active={playerStatus.includes(CardAbilities.nextCardTrump)} />,
+      <HangSaverIcon key='hs' active={playerStatus.includes(CardAbilities.hangSaver)} />,
+      <ChooseStarterIcon key='cs' active={playerStatus.includes(CardAbilities.chooseStarter)} />,
+      <AllyPlaysLastIcon key='apl' active={playerStatus.includes(CardAbilities.allyPlaysLast)} />,
+    ];
+  }, [playerStatus, dealerData, turnPlayerData, playerData]);
+
+
   return (
     <div className={className}>
-      <DealerIcon active={dealerData && playerData.id == dealerData.id} />
-      <TurnIcon active={turnPlayerData && playerData.id == turnPlayerData.id} />
-      <TwoWinGameIcon active={playerStatus.includes(CardAbilities.twoWinGame)} />
-      <NextCardTrumpIcon active={playerStatus.includes(CardAbilities.nextCardTrump)} />
-      <HangSaverIcon active={playerStatus.includes(CardAbilities.hangSaver)} />
-      <ChooseStarterIcon active={playerStatus.includes(CardAbilities.chooseStarter)} />
-      <AllyPlaysLastIcon active={playerStatus.includes(CardAbilities.allyPlaysLast)} />
+      <AnimatePresence>
+        {
+          // Need to do filter otherwise spring animation to close gaps between icons won't work
+          activeItems.filter((item) => item.props.active).map((item) => item)
+        }
+      </AnimatePresence>
     </div>
   );
 }
