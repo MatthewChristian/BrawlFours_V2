60c7aab4dc96b9931cd68fd00c7d6ce02d2eff8e
MatthewChristian <matthewjlchristian@yahoo.com>
2025-07-19 18:13:39 -0400

    Don't emit extra init game data if game has not started

--- server.ts
+++ server.ts
@@ -217,6 +217,10 @@ function emitInitGameData(data: BasicRoomInput, gameSocket: Socket) {
 
   playersInRoom(data);
 
+  if (!roomUsers[data.roomId].gameStarted) {
+    return;
+  }
+
   playerCards(data, gameSocket);
   teammateCards(data, gameSocket);
 
